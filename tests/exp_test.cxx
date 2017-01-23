893a98b62cbeb1e2ccefe70308d2139cf7d1b4bc
Travis Desell <travis.desell@gmail.com>
2017-01-23 17:36:08 -0600

    Using custom exp function

--- tests/exp_test.cxx
+++ tests/exp_test.cxx
@@ -3,7 +3,7 @@
 
 #include <cmath>
 
-#define iterations 30
+#define iterations 101
 
 double exact_exp(double z) {
     // exp(x) = sum (k = 0 to inf) z^k/k!
@@ -23,24 +23,64 @@ double exact_exp(double z) {
 
 
 int main(int argc, char **argv) {
-    double value = 1.3;
-    double modifier = 1.19;
+    double value;
+    double modifier;
 
+    /*
+    std::cout << "TESTING LARGE VALUES!" << std::endl;
+    value = 1.3;
+    modifier = 1.19;
     for (int i = 0; i < 50; i++) {
-        std::cout << std::hexfloat << std::exp(value) << std::endl;
-        std::cout << std::hexfloat << exact_exp(value) << std::endl;
-        std::cout << std::defaultfloat << std::setprecision(20) << std::exp(value) << std::endl;
-        std::cout << std::defaultfloat << std::setprecision(20) << exact_exp(value) << std::endl;
+        std::cout << "std   " << std::hexfloat << std::exp(value) << std::endl;
+        std::cout << "exact " << std::hexfloat << exact_exp(value) << std::endl;
+        std::cout << "std   " << std::defaultfloat << std::setprecision(20) << std::exp(value) << std::endl;
+        std::cout << "exact " << std::defaultfloat << std::setprecision(20) << exact_exp(value) << std::endl;
+        std::cout << std::endl;
         value *= modifier;
     }
+    std::cout << std::endl;
+    */
 
+    std::cout << "TESTING NEGATIVE LARGE VALUES!" << std::endl;
+    value = -1.3;
+    modifier = 1.19;
+    for (int i = 0; i < 50; i++) {
+        std::cout << "value " << std::defaultfloat << value << std::endl;
+        std::cout << "std   " << std::hexfloat << std::exp(value) << std::endl;
+        std::cout << "exact " << std::hexfloat << exact_exp(value) << std::endl;
+        std::cout << "std   " << std::defaultfloat << std::setprecision(20) << std::exp(value) << std::endl;
+        std::cout << "exact " << std::defaultfloat << std::setprecision(20) << exact_exp(value) << std::endl;
+        std::cout << std::endl;
+        value *= modifier;
+    }
+    std::cout << std::endl;
+
+    /*
+    std::cout << "TESTING SMALL VALUES!" << std::endl;
     value = 0.13;
-    modifier = 0.79;
+    modifier = 0.079;
     for (int i = 0; i < 50; i++) {
-        std::cout << std::hexfloat << std::exp(value) << std::endl;
-        std::cout << std::hexfloat << exact_exp(value) << std::endl;
-        std::cout << std::defaultfloat << std::setprecision(20) << std::exp(value) << std::endl;
-        std::cout << std::defaultfloat << std::setprecision(20) << exact_exp(value) << std::endl;
+        std::cout << "std   " << std::hexfloat << std::exp(value) << std::endl;
+        std::cout << "exact " << std::hexfloat << exact_exp(value) << std::endl;
+        std::cout << "std   " << std::defaultfloat << std::setprecision(20) << std::exp(value) << std::endl;
+        std::cout << "exact " << std::defaultfloat << std::setprecision(20) << exact_exp(value) << std::endl;
+        std::cout << std::endl;
         value *= modifier;
     }
+    std::cout << std::endl;
+
+    std::cout << "TESTING SMALL NEGATIVE VALUES!" << std::endl;
+    value = -0.13;
+    modifier = 0.079;
+    for (int i = 0; i < 50; i++) {
+        std::cout << "std   " << std::hexfloat << std::exp(value) << std::endl;
+        std::cout << "exact " << std::hexfloat << exact_exp(value) << std::endl;
+        std::cout << "std   " << std::defaultfloat << std::setprecision(20) << std::exp(value) << std::endl;
+        std::cout << "exact " << std::defaultfloat << std::setprecision(20) << exact_exp(value) << std::endl;
+        std::cout << std::endl;
+        value *= modifier;
+    }
+    std::cout << std::endl;
+    */
+
 }
