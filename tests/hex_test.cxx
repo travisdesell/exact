b6c966f44f85b0f6f47f9cd44902d45ee7b49ba8
Travis Desell <travis.desell@gmail.com>
2017-01-23 14:00:30 -0500

    Updates testing hex_test

--- tests/hex_test.cxx
+++ tests/hex_test.cxx
@@ -1,18 +1,27 @@
+#include <cstdio>
+
 #include<iostream>
 using std::cout;
 using std::endl;
+using std::hexfloat;
+using std::defaultfloat;
 
 #include <iomanip>
 using std::setprecision;
 
+#include <sstream>
+using std::ostringstream;
+using std::istringstream;
+
+#include <string>
+using std::string;
+
 int main(int argc, char** argv) {
     
-    double value = 2342.092834927394;
+    double value = 2342.092834927394e-100;
 
-    cout << std::hex << value << endl;
-    cout << value << endl;
-    cout << std::dec << value << endl;
-    cout << value << endl;
+    cout << setprecision(20) << value << endl;
+    cout << hexfloat << value << " stuff " << value << endl;
 
     printf("hex: %a\n", value);
     printf("hex: %A\n", value);
@@ -24,5 +33,17 @@ int main(int argc, char** argv) {
     sprintf(test, "%a\n", value);
     sscanf(test, "%la", &in_value);
 
-    cout << "in value: " << setprecision(20) << in_value << endl;
+    cout << "in value: " << defaultfloat << setprecision(20) << in_value << endl;
+
+    ostringstream oss;
+    oss << hexfloat << value;
+
+    istringstream iss(oss.str());
+
+    double in_value_2 = 5;
+    iss >> in_value_2;
+    cout << "in value 2: " << setprecision(20) << in_value_2 << endl;
+
+    double val_str = stod(oss.str());
+    cout << "val_str: " << val_str << endl;
 }
