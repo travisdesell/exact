180cc9b0207301f1a2eb5bd0ed240f494847094c
Travis Desell <travis.desell@gmail.com>
2017-03-18 15:00:04 -0500

    Updated copy scripts

--- copy_binary_osx.sh
+++ copy_binary_osx.sh
@@ -1,1 +1,1 @@
-scp ~/Dropbox/code/exact/build/client/exact_client_$1 tdesell@csgrid.org:/projects/csg/apps/exact/$1/x86_64-apple-darwin/exact_client_$1_x86_64-apple-darwin
+scp ~/Dropbox/code/exact/build/client/exact_client_$1 tdesell@csgrid.org:/projects/csg/apps/exact2/$1/x86_64-apple-darwin/exact_client_$1_x86_64-apple-darwin
