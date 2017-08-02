b9178051ed5a96a753680504b427eada628326e9
Travis Desell <travis.desell@gmail.com>
2017-08-01 23:54:47 -0500

    Now deleting previous best predictions genome if it was not in the population

--- strategy/exact.cxx
+++ strategy/exact.cxx
@@ -1283,13 +1283,21 @@ bool EXACT::insert_genome(CNN_Genome* genome) {
         }
 
         if (total_genome_predictions > total_best_genome_predictions) {
+            CNN_Genome *previous_best = best_predictions_genome;
+ 
             best_predictions_genome = genome;
 
 #ifdef _MYSQL_
             genome->export_to_database(id);
+            best_predictions_genome_id = genome->get_genome_id();
 #endif
 
-            best_predictions_genome_id = genome->get_genome_id();
+            //delete best_predictions_genome if it is not in the population
+            CNN_Genome *worst = genomes.back();
+            if (worst->get_generalizability_error() < previous_best->get_generalizability_error()) {
+                delete previous_best;
+            }
+
             was_best_predictions_genome = true;
         }
     }
