c417474ae009fee633e7e60e6bf20bf65a96322b
Travis Desell <tdesell@Traviss-MacBook-Pro.local>
2018-07-19 21:28:03 +0900

    Added file to evaluate RNN

---
+++ rnn_examples/evaluate_rnn.cxx
@@ -0,0 +1,142 @@
+#include <chrono>
+
+#include <condition_variable>
+using std::condition_variable;
+
+#include <iomanip>
+using std::setw;
+
+#include <iostream>
+using std::cerr;
+using std::cout;
+using std::endl;
+
+#include <mutex>
+using std::mutex;
+
+#include <string>
+using std::string;
+
+#include <thread>
+using std::thread;
+
+#include <vector>
+using std::vector;
+
+#include "common/arguments.hxx"
+
+#include "rnn/rnn_genome.hxx"
+
+#include "time_series/time_series.hxx"
+
+
+vector<string> arguments;
+
+vector< vector< vector<double> > > training_inputs;
+vector< vector< vector<double> > > training_outputs;
+vector< vector< vector<double> > > testing_inputs;
+vector< vector< vector<double> > > testing_outputs;
+
+int main(int argc, char** argv) {
+    arguments = vector<string>(argv, argv + argc);
+
+    vector<string> training_filenames;
+    get_argument_vector(arguments, "--training_filenames", true, training_filenames);
+
+    vector<string> testing_filenames;
+    get_argument_vector(arguments, "--testing_filenames", true, testing_filenames);
+
+    int32_t time_offset = 1;
+    get_argument(arguments, "--time_offset", true, time_offset);
+
+    bool normalize = argument_exists(arguments, "--normalize");
+
+    string genome_filename;
+    get_argument(arguments, "--genome_file", true, genome_filename);
+
+    vector<string> input_parameter_names;
+    /*
+    input_parameter_names.push_back("indicated_airspeed");
+    input_parameter_names.push_back("msl_altitude");
+    input_parameter_names.push_back("eng_1_rpm");
+    input_parameter_names.push_back("eng_1_fuel_flow");
+    input_parameter_names.push_back("eng_1_oil_press");
+    input_parameter_names.push_back("eng_1_oil_temp");
+    input_parameter_names.push_back("eng_1_cht_1");
+    input_parameter_names.push_back("eng_1_cht_2");
+    input_parameter_names.push_back("eng_1_cht_3");
+    input_parameter_names.push_back("eng_1_cht_4");
+    input_parameter_names.push_back("eng_1_egt_1");
+    input_parameter_names.push_back("eng_1_egt_2");
+    input_parameter_names.push_back("eng_1_egt_3");
+    input_parameter_names.push_back("eng_1_egt_4");
+    */
+    
+    /*
+    input_parameter_names.push_back("par1");
+    input_parameter_names.push_back("par2");
+    input_parameter_names.push_back("par3");
+    input_parameter_names.push_back("par4");
+    input_parameter_names.push_back("par5");
+    input_parameter_names.push_back("par6");
+    input_parameter_names.push_back("par7");
+    input_parameter_names.push_back("par8");
+    input_parameter_names.push_back("par9");
+    input_parameter_names.push_back("par10");
+    input_parameter_names.push_back("par11");
+    input_parameter_names.push_back("par12");
+    input_parameter_names.push_back("par13");
+    input_parameter_names.push_back("par14");
+    input_parameter_names.push_back("vib");
+    */
+
+    input_parameter_names.push_back("Coyote-GROSS_GENERATOR_OUTPUT");
+    input_parameter_names.push_back("Coyote-Net_Unit_Generation");
+    input_parameter_names.push_back("Cyclone_-CYC__CONDITIONER_INLET_TEMP");
+    input_parameter_names.push_back("Cyclone_-CYC__CONDITIONER_OUTLET_TEMP");
+    input_parameter_names.push_back("Cyclone_-LIGNITE_FEEDER__RATE");
+    input_parameter_names.push_back("Cyclone_-CYC__TOTAL_COMB_AIR_FLOW");
+    input_parameter_names.push_back("Cyclone_-_MAIN_OIL_FLOW");
+    input_parameter_names.push_back("Cyclone_-CYCLONE__MAIN_FLM_INT");
+
+    vector<string> output_parameter_names;
+    //output_parameter_names.push_back("Cyclone_-_MAIN_OIL_FLOW");
+    output_parameter_names.push_back("Cyclone_-CYCLONE__MAIN_FLM_INT");
+
+    //output_parameter_names.push_back("vib");
+
+    //output_parameter_names.push_back("indicated_airspeed");
+    //output_parameter_names.push_back("eng_1_oil_press");
+    /*
+    output_parameter_names.push_back("msl_altitude");
+    output_parameter_names.push_back("eng_1_rpm");
+    output_parameter_names.push_back("eng_1_fuel_flow");
+    output_parameter_names.push_back("eng_1_oil_press");
+    output_parameter_names.push_back("eng_1_oil_temp");
+    output_parameter_names.push_back("eng_1_cht_1");
+    output_parameter_names.push_back("eng_1_cht_2");
+    output_parameter_names.push_back("eng_1_cht_3");
+    output_parameter_names.push_back("eng_1_cht_4");
+    output_parameter_names.push_back("eng_1_egt_1");
+    output_parameter_names.push_back("eng_1_egt_2");
+    output_parameter_names.push_back("eng_1_egt_3");
+    output_parameter_names.push_back("eng_1_egt_4");
+    */
+
+
+    load_time_series(training_filenames, testing_filenames, input_parameter_names, output_parameter_names, time_offset, training_inputs, training_outputs, testing_inputs, testing_outputs, normalize);
+
+    int number_inputs = training_inputs[0].size();
+    int number_outputs = training_outputs[0].size();
+
+    cout << "number_inputs: " << number_inputs << ", number_outputs: " << number_outputs << endl;
+
+    RNN_Genome *duplicate_genome = new RNN_Genome(genome_filename, true);
+
+    vector<double> duplicate_parameters = duplicate_genome->get_best_parameters();
+    cout << "MSE: " << duplicate_genome->get_mse(duplicate_parameters, testing_inputs, testing_outputs) << endl;
+    cout << "MAE: " << duplicate_genome->get_mae(duplicate_parameters, testing_inputs, testing_outputs) << endl;
+    duplicate_genome->write_predictions(testing_filenames, duplicate_parameters, testing_inputs, testing_outputs);
+
+    return 0;
+}
