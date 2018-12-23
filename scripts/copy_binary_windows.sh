08fd06a70020626894b896d9a2e424d1781ddf86
Travis Desell <travisdesell@Traviss-MacBook-Pro.local>
2018-12-23 10:57:48 -0500

    Added random scripts

---
+++ scripts/copy_binary_windows.sh
@@ -0,0 +1,5 @@
+scp ~/Code/boinc_2015/win_build/Build/Win32/Release/exact.exe tdesell@csgrid.org:/projects/csg/apps/exact_bn_sfmp/$1/windows_intelx86/exact_client_$1_windows_intelx86.exe
+scp ~/Code/boinc_2015/win_build/Build/x64/Release/exact.exe tdesell@csgrid.org:/projects/csg/apps/exact_bn_sfmp/$1/windows_x86_64/exact_client_$1_windows_x86_64.exe
+
+#scp ~/Code/boinc_2015/win_build/Build/Win32/Release/exact.exe tdesell@csgrid.org:/projects/csg/apps/exact_cifar/$1/windows_intelx86/exact_client_$1_windows_intelx86.exe
+#scp ~/Code/boinc_2015/win_build/Build/x64/Release/exact.exe tdesell@csgrid.org:/projects/csg/apps/exact_cifar/$1/windows_x86_64/exact_client_$1_windows_x86_64.exe
