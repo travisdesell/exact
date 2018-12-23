5d9ea3e5aa499576baee21c3586cf8bdafb75223
Travis Desell <travisdesell@Traviss-MacBook-Pro.local>
2018-12-23 10:57:32 -0500

    Added missing gradient test, fixed includes

--- image_tools/large_image_set.cxx
+++ image_tools/large_image_set.cxx
@@ -31,8 +31,8 @@ using std::vector;
 #include <sstream>
 using std::ostringstream;
 
-#include "/usr/local/Cellar/libtiff/4.0.10/include/tiff.h"
-#include "/usr/local/Cellar/libtiff/4.0.10/include/tiffio.h"
+#include "tiff.h"
+#include "tiffio.h"
 #endif
 
 #include "lodepng.h"
