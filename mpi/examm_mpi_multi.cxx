373f37ae6196acae616f3bd0d2f2f6fe39ec3e26
Travis Desell <travisdesell@Traviss-MacBook-Pro.local>
2019-09-24 16:36:31 -0400

    Added renamed EXAMM files

---
+++ mpi/examm_mpi_multi.cxx
@@ -0,0 +1,337 @@
+#include <chrono>
+
+#include <iomanip>
+using std::setw;
+using std::fixed;
+using std::setprecision;
+
+#include <iostream>
+using std::cerr;
+using std::cout;
+using std::endl;
+
+#include <mutex>
+using std::mutex;
+
+#include <string>
+using std::string;
+
+#include <thread>
+using std::thread;
+
+#include <vector>
+using std::vector;
+
+
+#include "mpi.h"
+
+#include "common/arguments.hxx"
+#include "common/files.hxx"
+
+#include "rnn/examm.hxx"
+
+#include "time_series/time_series.hxx"
+
+#define WORK_REQUEST_TAG 1
+#define GENOME_LENGTH_TAG 2
+#define GENOME_TAG 3
+#define TERMINATE_TAG 4
+
+mutex examm_mutex;
+
+vector<string> arguments;
+
+EXAMM *examm;
+
+vector< vector< vector<double> > > training_inputs;
+vector< vector< vector<double> > > training_outputs;
+vector< vector< vector<double> > > validation_inputs;
+vector< vector< vector<double> > > validation_outputs;
+
+void send_work_request(int target) {
+    int work_request_message[1];
+    work_request_message[0] = 0;
+    MPI_Send(work_request_message, 1, MPI_INT, target, WORK_REQUEST_TAG, MPI_COMM_WORLD);
+}
+
+void receive_work_request(int source) {
+    MPI_Status status;
+    int work_request_message[1];
+    MPI_Recv(work_request_message, 1, MPI_INT, source, WORK_REQUEST_TAG, MPI_COMM_WORLD, &status);
+}
+
+RNN_Genome* receive_genome_from(string name, int source) {
+    MPI_Status status;
+    int length_message[1];
+    MPI_Recv(length_message, 1, MPI_INT, source, GENOME_LENGTH_TAG, MPI_COMM_WORLD, &status);
+
+    int length = length_message[0];
+
+    cout << "[" << setw(10) << name << "] receiving genome of length: " << length << " from: " << source << endl;
+
+    char* genome_str = new char[length + 1];
+
+    cout << "[" << setw(10) << name << "] receiving genome from: " << source << endl;
+    MPI_Recv(genome_str, length, MPI_CHAR, source, GENOME_TAG, MPI_COMM_WORLD, &status);
+
+    genome_str[length] = '\0';
+
+    //cout << "genome_str:" << endl << genome_str << endl;
+
+    RNN_Genome* genome = new RNN_Genome(genome_str, length, false);
+
+    delete [] genome_str;
+    return genome;
+}
+
+void send_genome_to(string name, int target, RNN_Genome* genome) {
+    char *byte_array;
+    int32_t length;
+
+    genome->write_to_array(&byte_array, length);
+
+    cout << "[" << setw(10) << name << "] sending genome of length: " << length << " to: " << target << endl;
+
+    int length_message[1];
+    length_message[0] = length;
+    MPI_Send(length_message, 1, MPI_INT, target, GENOME_LENGTH_TAG, MPI_COMM_WORLD);
+
+    cout << "[" << setw(10) << name << "] sending genome to: " << target << endl;
+    MPI_Send(byte_array, length, MPI_CHAR, target, GENOME_TAG, MPI_COMM_WORLD);
+}
+
+void send_terminate_message(int target) {
+    int terminate_message[1];
+    terminate_message[0] = 0;
+    MPI_Send(terminate_message, 1, MPI_INT, target, TERMINATE_TAG, MPI_COMM_WORLD);
+}
+
+void receive_terminate_message(int source) {
+    MPI_Status status;
+    int terminate_message[1];
+    MPI_Recv(terminate_message, 1, MPI_INT, source, TERMINATE_TAG, MPI_COMM_WORLD, &status);
+}
+
+void master(int max_rank) {
+    string name = "master";
+
+    int terminates_sent = 0;
+
+    while (true) {
+        //wait for a incoming message
+        MPI_Status status;
+        MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        int source = status.MPI_SOURCE;
+        int tag = status.MPI_TAG;
+        cout << "[" << setw(10) << name << "] probe returned message from: " << source << " with tag: " << tag << endl;
+
+
+        //if the message is a work request, send a genome
+
+        if (tag == WORK_REQUEST_TAG) {
+            receive_work_request(source);
+
+            examm_mutex.lock();
+            RNN_Genome *genome = examm->generate_genome();
+            examm_mutex.unlock();
+
+            if (genome == NULL) { //search was completed if it returns NULL for an individual
+                //send terminate message
+                cout << "[" << setw(10) << name << "] terminating worker: " << source << endl;
+                send_terminate_message(source);
+                terminates_sent++;
+
+                cout << "[" << setw(10) << name << "] sent: " << terminates_sent << " terminates of: " << (max_rank - 1) << endl;
+                if (terminates_sent >= max_rank - 1) return;
+
+            } else {
+                //genome->write_to_file( examm->get_output_directory() + "/before_send_gen_" + to_string(genome->get_generation_id()) );
+
+                //send genome
+                cout << "[" << setw(10) << name << "] sending genome to: " << source << endl;
+                send_genome_to(name, source, genome);
+
+                //delete this genome as it will not be used again
+                delete genome;
+            }
+        } else if (tag == GENOME_LENGTH_TAG) {
+            cout << "[" << setw(10) << name << "] received genome from: " << source << endl;
+            RNN_Genome *genome = receive_genome_from(name, source);
+
+            examm_mutex.lock();
+            examm->insert_genome(genome);
+            examm_mutex.unlock();
+
+            //this genome will be deleted if/when removed from population
+        } else {
+            cerr << "[" << setw(10) << name << "] ERROR: received message with unknown tag: " << tag << endl;
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+}
+
+void worker(int rank) {
+    string name = "worker_" + to_string(rank);
+
+    while (true) {
+        cout << "[" << setw(10) << name << "] sending work request!" << endl;
+        send_work_request(0);
+        cout << "[" << setw(10) << name << "] sent work request!" << endl;
+
+        MPI_Status status;
+        MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        int tag = status.MPI_TAG;
+
+        cout << "[" << setw(10) << name << "] probe received message with tag: " << tag << endl;
+
+        if (tag == TERMINATE_TAG) {
+            cout << "[" << setw(10) << name << "] received terminate tag!" << endl;
+            receive_terminate_message(0);
+            break;
+
+        } else if (tag == GENOME_LENGTH_TAG) {
+            cout << "[" << setw(10) << name << "] received genome!" << endl;
+            RNN_Genome* genome = receive_genome_from(name, 0);
+
+            genome->backpropagate_stochastic(training_inputs, training_outputs, validation_inputs, validation_outputs);
+
+            send_genome_to(name, 0, genome);
+
+            delete genome;
+        } else {
+            cerr << "[" << setw(10) << name << "] ERROR: received message with unknown tag: " << tag << endl;
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, max_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &max_rank);
+
+    arguments = vector<string>(argv, argv + argc);
+
+    int32_t population_size;
+    get_argument(arguments, "--population_size", true, population_size);
+
+    int32_t number_islands;
+    get_argument(arguments, "--number_islands", true, number_islands);
+
+    int32_t max_genomes;
+    get_argument(arguments, "--max_genomes", true, max_genomes);
+
+    int32_t bp_iterations;
+    get_argument(arguments, "--bp_iterations", true, bp_iterations);
+
+    double learning_rate = 0.001;
+    get_argument(arguments, "--learning_rate", false, learning_rate);
+
+    double high_threshold = 1.0;
+    bool use_high_threshold = get_argument(arguments, "--high_threshold", false, high_threshold);
+
+    double low_threshold = 0.05;
+    bool use_low_threshold = get_argument(arguments, "--low_threshold", false, low_threshold);
+
+    double dropout_probability = 0.0;
+    bool use_dropout = get_argument(arguments, "--dropout_probability", false, dropout_probability);
+
+    int32_t repeats;
+    get_argument(arguments, "--repeats", true, repeats);
+
+
+    string output_directory = "";
+    get_argument(arguments, "--output_directory", false, output_directory);
+
+    //mkpath(output_directory.c_str(), 0777);
+
+    TimeSeriesSets *time_series_sets = NULL;
+    
+    if (rank == 0) {
+        //only have the master process be verbose
+        time_series_sets = TimeSeriesSets::generate_from_arguments(arguments, true);
+    } else {
+        time_series_sets = TimeSeriesSets::generate_from_arguments(arguments, false);
+    }
+
+    int32_t time_offset = 1;
+    get_argument(arguments, "--time_offset", true, time_offset);
+
+    int fold_size = 2;
+    get_argument(arguments, "--fold_size", true, fold_size);
+
+    vector<string> possible_node_types;
+    get_argument_vector(arguments, "--possible_node_types", true, possible_node_types);
+
+
+    for (int32_t i = 0; i < time_series_sets->get_number_series(); i += fold_size) {
+        vector<int> training_indexes;
+        vector<int> test_indexes;
+
+        for (uint32_t j = 0; j < time_series_sets->get_number_series(); j += fold_size) {
+            if (j == i) {
+                for (int k = 0; k < fold_size; k++) {
+                    test_indexes.push_back(j + k);
+                }
+            } else {
+                for (int k = 0; k < fold_size; k++) {
+                    training_indexes.push_back(j + k);
+                }
+            }
+        }
+
+        time_series_sets->set_training_indexes(training_indexes);
+        time_series_sets->set_test_indexes(test_indexes);
+
+        //time_series_sets->export_training_series(time_offset, training_inputs, training_outputs);
+        //time_series_sets->export_test_series(time_offset, validation_inputs, validation_outputs);
+
+        string slice_output_directory = output_directory + "/slice_" + to_string(i);
+        mkpath(slice_output_directory.c_str(), 0777);
+        ofstream slice_times_file(output_directory + "/slice_" + to_string(i) + "_runtimes.csv");
+
+        for (int k = 0; k < repeats; k++) {
+            string current_output_directory = slice_output_directory + "/repeat_" + to_string(k);
+            mkpath(current_output_directory.c_str(), 0777);
+
+            if (rank == 0) {
+                examm = new EXAMM(population_size, number_islands, max_genomes, time_series_sets->get_input_parameter_names(), time_series_sets->get_output_parameter_names(), time_series_sets->get_normalize_mins(), time_series_sets->get_normalize_maxs(), bp_iterations, learning_rate, use_high_threshold, high_threshold, use_low_threshold, low_threshold, use_dropout, dropout_probability, current_output_directory);
+                examm->set_possible_node_types(possible_node_types);
+
+                std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+                master(max_rank);
+                std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+                long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+                examm->write_memory_log(current_output_directory + "/memory_fitness_log.csv");
+
+                slice_times_file << milliseconds << endl;
+
+                RNN_Genome *best_genome = examm->get_best_genome();
+
+                string binary_file = slice_output_directory + "/repeat_best_" + to_string(k) + ".bin";
+                string graphviz_file = slice_output_directory + "/repeat_best_" + to_string(k) + ".gv";
+
+                cout << "writing best genome to '" << binary_file << "' and '" << graphviz_file << "'" << endl;
+                best_genome->write_to_file(binary_file, false);
+                best_genome->write_graphviz(graphviz_file);
+
+                delete examm;
+            } else {
+                worker(rank);
+            }
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            cout << "rank " << rank << " completed slice " << i << " of " << time_series_sets->get_number_series() << " repeat " << k << " of " << repeats << endl;
+        }
+
+        slice_times_file.close();
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
