ca178e8d43f10e551bd6641ecab0385020b90fb7
Joshua Karns <jkarns275@gmail.com>
2019-09-27 13:54:32 -0400

    Preparing CLI for use in this rec_depth_population_sampling experiment

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 import subprocess
+import sys
 
 import toml
 
 from examm_task import ExammTask
 
+USAGE = """
+Usage:
+python3 cli [config_file_path]
+"""
 
-with open('cli/example_config.toml') as f:
-    text = f.read()
+def main():
+    args = sys.argv
 
-task = ExammTask(toml.loads(text), "junk:")
-command = task.to_command()
-print(command)
-subprocess.call(command)
+    if len(args) < 2:
+        print(USAGE)
+        print("You must provide a configuration file")
+        return
+
+    config_file_path = args[1]
+    try:
+        with open(config_file_path) as f:
+            text = f.read()
+    except FileNotFoundError as e:
+        print(f"Could not find configuration file '{config_file_path}'.")
+        print(f"Encountered error: {str(e)}")
+        return
+    task = ExammTask(toml.loads(text), "junk:")
+    command = task.to_command()
+    print(command)
+    subprocess.call(command)
+
+if __name__ == "__main__":
+    main()
