632c8dfbef5d625c02bea879b85089a78d3499b6
cathybgyz <zlyu01@syr.edu>
2019-10-24 00:27:06 -0400

    add check points to the plot and sample fitness file to plot worst/best fitness

--- visualization/plot_worst_best.py
+++ visualization/plot_worst_best.py
@@ -1,8 +1,7 @@
-# plot worst/best fitness of each island, 
+# plot worst/best fitness of each island,
 # fitness file must have worst and best fitness of each island
-# only one argument: fitness file address
-#
-# Zimeng Lyu 
+# arguments: fit_file: fiteness file address
+#            plot_identifier: name prefix for the saved plot
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,43 +10,50 @@ import os
 import pandas as pd
 
 fit_file = sys.argv[1]
+plot_identifier = sys.argv[2]
+N_row = 2
+N_column = 5
+N_check = 200
+plot_dir= "./plots"
 
-N_row=2
-N_column=5
 def get_ratio(n, data):
     ratio = []
     for i in range(n):
         # best/worst
-        temp = np.divide(data[:, 2*i], data[:, 2*i+1]).reshape((-1,1))
+        temp = np.divide(data[:, 2*i], data[:, 2*i+1]).reshape((-1, 1))
         ratio.append(temp)
     length = temp.shape[0]
-    ratio=np.asarray(ratio).reshape((n,length))
+    ratio = np.asarray(ratio).reshape((n, length))
 
     return ratio
 
-def plot_island(ratio):
+
+def plot_island(ratio, x_check):
     plt.figure()
     k = 0
     for i in range(N_row):
         for j in range(N_column):
-            plt.subplot2grid((2, 5), (i, j))
+            plt.subplot2grid((N_row, N_column), (i, j))
             plt.plot(ratio[k, :])
+            plt.scatter(x=x_check, y=ratio[k, x_check],c='r',marker='o')
             plt.grid(True)
-            plt.title('island_'+str(k))
             k = k+1
-    # plt.savefig("./worst_best.png")
+
+    if not os.path.exists(plot_dir):
+        os.mkdir(plot_dir)
+    plt.savefig(plot_dir+"/worst_best_" + plot_identifier + ".png")
     plt.show()
 
-    # plt.savefig("./worst_best.png")
 def main():
 
-    in_data=pd.read_csv(fit_file)
+    in_data = pd.read_csv(fit_file)
     fit_data = in_data.values[:, 8:]
-    # print(fit_data.shape)
-    n=int(fit_data.shape[1]/2)
-    ratio=get_ratio(n, fit_data)
+    n = int(fit_data.shape[1]/2)
+    x_domain_max = fit_data.shape[0]
+    x_check = np.arange(0, x_domain_max, N_check)
+    ratio = get_ratio(n, fit_data)
     print(ratio.shape)
-    plot_island(ratio)
+    plot_island(ratio, x_check)
 
 
 if __name__ == "__main__":
