d50f7b9afc5f7f336fe3e4b830abf055835c157a
Travis Desell <travisdesell@traviss-mbp.wireless.rit.edu>
2019-10-24 15:37:27 -0400

    Removed the first genome for repopulation being the minimal genome

--- rnn/examm.cxx
+++ rnn/examm.cxx
@@ -677,61 +677,32 @@ RNN_Genome* EXAMM::generate_genome() {
         //select two other islands (non-overlapping) at random, and select genomes
         //from within those islands and generate a child via crossover
 
-        if (genomes[island].size() == 0) {
-            //this is the first genome to be generated
-            //generate minimal genome, insert it into the population
-            genome = create_ff(number_inputs, 0, 0, number_outputs, 0);
-            genome->set_island(island);
-            genome->set_parameter_names(input_parameter_names, output_parameter_names);
+        while (genome == NULL) {
+            int32_t parent_island1;
+            do {
+                parent_island1 = genomes.size() * rng_0_1(generator);
+            } while (island_states[parent_island1] != ISLAND_FILLED);
+
+            int32_t parent_island2;
+            do {
+                parent_island2 = genomes.size() * rng_0_1(generator);
+            } while (   parent_island1 == parent_island2 
+                     || island_states[parent_island2] != ISLAND_FILLED);
+
+            RNN_Genome* genome1 = genomes[parent_island1][genomes[parent_island1].size() * rng_0_1(generator)];
+            RNN_Genome* genome2 = genomes[parent_island2][genomes[parent_island2].size() * rng_0_1(generator)];
+
+            genome = crossover(genome1, genome2);
             genome->set_normalize_bounds(normalize_mins, normalize_maxs);
+            genome->set_island(island);
 
-            edge_innovation_count = genome->edges.size() + genome->recurrent_edges.size();
-            node_innovation_count = genome->nodes.size();
-
-            genome->set_generated_by("initial");
-            initialize_genome_parameters(genome);
-
-            //insert a copy of it into the population so
-            //additional requests can mutate it
-            genome->initialize_randomly();
-            double _mu, _sigma;
-            cout << "getting mu/sigma after random initialization!" << endl;
-            genome->get_mu_sigma(genome->best_parameters, _mu, _sigma);
-
-            genome->best_validation_mse = EXAMM_MAX_DOUBLE;
-            genome->best_validation_mae = EXAMM_MAX_DOUBLE;
-            genome->best_parameters.clear();
-            //genome->clear_generated_by();
-
-            insert_genome(genome->copy());
-        } else {
-            
-            while (genome == NULL) {
-                int32_t parent_island1;
-                do {
-                    parent_island1 = genomes.size() * rng_0_1(generator);
-                } while (island_states[parent_island1] != ISLAND_FILLED);
-                
-                int32_t parent_island2;
-                do {
-                    parent_island2 = genomes.size() * rng_0_1(generator);
-                } while (   parent_island1 == parent_island2 
-                         || island_states[parent_island2] != ISLAND_FILLED);
-
-                RNN_Genome* genome1 = genomes[parent_island1][genomes[parent_island1].size() * rng_0_1(generator)];
-                RNN_Genome* genome2 = genomes[parent_island2][genomes[parent_island2].size() * rng_0_1(generator)];
-
-                genome = crossover(genome1, genome2);
-                genome->set_normalize_bounds(normalize_mins, normalize_maxs);
-                genome->set_island(island);
-
-                if (genome->outputs_unreachable()) {
-                    //no path from at least one input to the outputs
-                    delete genome;
-                    genome = NULL;
-                }
+            if (genome->outputs_unreachable()) {
+                //no path from at least one input to the outputs
+                delete genome;
+                genome = NULL;
             }
         }
+
     } else {
         cerr << "ERROR: unknown island state (" << island_states[island] << ")" << endl;
         cerr << "This should never happen!" << endl;
