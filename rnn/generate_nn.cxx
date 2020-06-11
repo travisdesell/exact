22ea189736e917388c15967c3ec5ecdbcac5ba3a
BumbleBEE <vinay.vjjain.jai4@gmail.com>
2020-06-11 13:53:45 -0400

    added enarc files

--- rnn/generate_nn.cxx
+++ rnn/generate_nn.cxx
@@ -10,6 +10,7 @@ using std::vector;
 #include "rnn/gru_node.hxx"
 #include "rnn/mgu_node.hxx"
 #include "rnn/lstm_node.hxx"
+#include "rnn/enarc_node.hxx"
 #include "rnn/rnn_edge.hxx"
 #include "rnn/rnn_genome.hxx"
 #include "rnn/rnn_node.hxx"
@@ -309,6 +310,51 @@ RNN_Genome* create_gru(const vector<string> &input_parameter_names, int number_h
     for (int32_t i = 0; i < number_hidden_layers; i++) {
         for (int32_t j = 0; j < number_hidden_nodes; j++) {
             GRU_Node *node = new GRU_Node(++node_innovation_count, HIDDEN_LAYER, current_layer);
+            rnn_nodes.push_back(node);
+            layer_nodes[current_layer].push_back(node);
+
+            for (uint32_t k = 0; k < layer_nodes[current_layer - 1].size(); k++) {
+                rnn_edges.push_back(new RNN_Edge(++edge_innovation_count, layer_nodes[current_layer - 1][k], node));
+            }
+        }
+        current_layer++;
+    }
+
+    for (int32_t i = 0; i < output_parameter_names.size(); i++) {
+        RNN_Node *output_node = new RNN_Node(++node_innovation_count, OUTPUT_LAYER, current_layer, SIMPLE_NODE, output_parameter_names[i]);
+        rnn_nodes.push_back(output_node);
+
+        for (uint32_t k = 0; k < layer_nodes[current_layer - 1].size(); k++) {
+            rnn_edges.push_back(new RNN_Edge(++edge_innovation_count, layer_nodes[current_layer - 1][k], output_node));
+        }
+    }
+
+    RNN_Genome *genome = new RNN_Genome(rnn_nodes, rnn_edges, recurrent_edges);
+    genome->set_parameter_names(input_parameter_names, output_parameter_names);
+    return genome;
+}
+
+RNN_Genome* create_enarc(const vector<string> &input_parameter_names, int number_hidden_layers, int number_hidden_nodes, const vector<string> &output_parameter_names, int max_recurrent_depth) {
+    Log::debug("creating ENARC network with inputs: %d, hidden: %dx%d, outputs: %d, max recurrent depth: %d\n", input_parameter_names.size(), number_hidden_layers, number_hidden_nodes, output_parameter_names.size(), max_recurrent_depth);
+    vector<RNN_Node_Interface*> rnn_nodes;
+    vector< vector<RNN_Node_Interface*> > layer_nodes(2 + number_hidden_layers);
+    vector<RNN_Edge*> rnn_edges;
+    vector<RNN_Recurrent_Edge*> recurrent_edges;
+
+    int node_innovation_count = 0;
+    int edge_innovation_count = 0;
+    int current_layer = 0;
+
+    for (int32_t i = 0; i < input_parameter_names.size(); i++) {
+        RNN_Node *node = new RNN_Node(++node_innovation_count, INPUT_LAYER, current_layer, SIMPLE_NODE, input_parameter_names[i]);
+        rnn_nodes.push_back(node);
+        layer_nodes[current_layer].push_back(node);
+    }
+    current_layer++;
+
+    for (int32_t i = 0; i < number_hidden_layers; i++) {
+        for (int32_t j = 0; j < number_hidden_nodes; j++) {
+            ENARC_Node *node = new ENARC_Node(++node_innovation_count, HIDDEN_LAYER, current_layer);
             rnn_nodes.push_back(node);
             layer_nodes[current_layer].push_back(node);
 
--- time_series/time_series.cxx
+++ time_series/time_series.cxx
@@ -1,4 +1,4 @@
-#include <cmath>
+    #include <cmath>
 
 #include <algorithm>
 using std::find;
