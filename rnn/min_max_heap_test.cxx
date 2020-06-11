9c70a6eba168ace9498613a624e8aea996c81978
Joshua Karns <jkarns275@gmail.com>
2020-06-11 12:58:24 -0400

    Modified min_max_heap_test to test the speed of min_max_heap compared to a vector

--- rnn/min_max_heap_test.cxx
+++ rnn/min_max_heap_test.cxx
@@ -3,13 +3,15 @@
 #include <cstdlib>
 #include <iostream>
 #include <stdlib.h>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
 
 #include "min_max_heap.hxx"
 
 
-#define N 500
+#define N 5000000
 void init(vector<int> *v) {
     v->clear();
     for (int i = 0; i < N; i+=1) {
@@ -90,27 +92,72 @@ void test_replace_max(min_max_heap<int> heap, vector<int> &numbers, vector<int> 
     should_be_eq(sorted, should_be_sorted, "test_replace_max");
 }
 
+#define POPULATION_SIZE 5
+double examm_test_heap(min_max_heap<int> heap, vector<int> &numbers) {
+    heap.clear();
+
+    auto start = high_resolution_clock::now();
+    for (int32_t i = 0; i < numbers.size(); i += 1) {
+        if (heap.size() >= POPULATION_SIZE) {
+            if (heap.find_max() > numbers[i])
+                heap.replace_max(numbers[i]);
+        } else {
+            heap.enqueue(numbers[i]);
+        }
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    long nanos = duration.count();
+    double dnanos = (double) nanos;
+    return dnanos * 1e-9;
+}
+
+inline void sorted_insert(vector<int> &population, int n) {
+    population.insert(std::lower_bound(population.begin(), population.end(), n), n);
+}
+
+double examm_test_vector(vector<int> &numbers) {
+    vector<int> population;
+    population.reserve(POPULATION_SIZE);
+
+    auto start = high_resolution_clock::now();
+    for (int32_t i = 0; i < numbers.size(); i += 1) {
+        if (population.size() >= POPULATION_SIZE) {
+            if (population[POPULATION_SIZE - 1] > numbers[i]) {
+                population.pop_back();
+                sorted_insert(population, numbers[i]);
+            }
+        } else {
+            sorted_insert(population, numbers[i]);
+        }
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    long nanos = duration.count();
+    double dnanos = (double) nanos;
+    return dnanos * 1e-9;
+}
+
 int main() {
     function<bool (const int&, const int&)> less_than =
         [](const int& a, const int& b) {
             return a < b;
         };
     min_max_heap<int> heap(less_than);
-
     vector<int> numbers;
     init(&numbers);
+    double time_vec = examm_test_vector(numbers);
+    cout << "vector took " << time_vec << "s" << endl;
 
-    vector<int> sorted(numbers);
-    std::sort(sorted.begin(), sorted.end());
-    
-    test_pop_min(heap, numbers, sorted);
+    double time_heap = examm_test_heap(heap, numbers);
+    cout << "heap took " << time_heap << "s" << endl;
 
-    std::sort(sorted.begin(), sorted.end(), [](int i, int j) { return i > j; });
+    //// std::sort(sorted.begin(), sorted.end(), [](int i, int j) { return i > j; });
 
-    test_pop_max(heap, numbers, sorted);
-    
-    for (int i = 0; i < N; i++)
-        sorted[i] -= 1;
-    
-    test_replace_max(heap, numbers, sorted);
+    //test_pop_max(heap, numbers, sorted);
+    //
+    //for (int i = 0; i < N; i++)
+    //    sorted[i] -= 1;
+    //
+    //test_replace_max(heap, numbers, sorted);
 }
