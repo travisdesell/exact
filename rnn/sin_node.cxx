c519474dabc4cd236cc687d104043d06cf77de83
jared-m-murphy <jared.m.murphy@protonmail.com>
2023-10-25 15:53:40 -0400

    sin_node added as child class of rnn_node. derivitive and activation functions added rnn_node

---
+++ rnn/sin_node.cxx
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <vector>
+using std::vector;
+
+#include "common/log.hxx"
+#include "sin_node.hxx"
+
+SIN_Node::SIN_Node(int32_t _innovation_number, int32_t _layer_type, double _depth)
+    : RNN_Node(_innovation_number, _layer_type, _depth, SIN_NODE), bias(0), dbias(0){
+    // node type will be simple, jordan or elman
+    Log::trace("created node: %d, layer type: %d, node type: %d\n", innovation_number, layer_type, node_type);
+}
+
+SIN_Node::~SIN_Node() {
+}
+
+double SIN_Node::activation_function(double input){
+    return sin(input);
+}
+
+double SIN_Node::derivative_function(double input){
+    return cos(input);
+}
+
+
