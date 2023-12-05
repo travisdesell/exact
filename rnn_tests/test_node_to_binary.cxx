37807d66a3413bdc143d04b651962e3707e2f6d0
jared-m-murphy <jared.m.murphy@protonmail.com>
2023-12-05 11:14:04 -0500

    updated test_node_to_binary

--- rnn_tests/test_node_to_binary.cxx
+++ rnn_tests/test_node_to_binary.cxx
@@ -13,6 +13,8 @@ using std::string;
 #include <vector>
 using std::vector;
 
+#include <iostream>
+
 #include "common/arguments.hxx"
 #include "common/log.hxx"
 #include "gradient_test.hxx"
@@ -47,54 +49,79 @@ int main(int argc, char** argv) {
     int32_t max_recurrent_depth = 3;
     Log::info("testing with max recurrent depth: %d\n", max_recurrent_depth);
 
-    inputs.resize(1);
-    outputs.resize(1);
+    inputs.resize(3);
+    outputs.resize(3);
+
+    vector<string> inputs3{"input 1", "input 2", "input 3"};
+    vector<string> outputs3{"output 1", "output 2", "input 3"};
+    Log::info("testing with 3 input nodes, 3 output nodes\n");
 
     generate_random_vector(input_length, inputs[0]);
     generate_random_vector(input_length, outputs[0]);
+    generate_random_vector(input_length, inputs[1]);
+    generate_random_vector(input_length, outputs[1]);
+    generate_random_vector(input_length, inputs[2]);
+    generate_random_vector(input_length, outputs[2]);
 
-    vector<string> inputs1{"input 1"};
-    vector<string> outputs1{"output 1"};
+    
 
     if (hidden_node_type.compare("sin") == 0) {
         Log::info("TESTING SIN!!!\n");
-        genome_original = create_sin(inputs1, 1, 2, outputs1, max_recurrent_depth, weight_rules);
+        genome_original = create_sin(inputs3, 1, 5, outputs3, max_recurrent_depth, weight_rules);
+        Log::info("testing with 1 hidden layer, 5 sin nodes\n");
     }
-    string path = "/Users/jaredmurphy/exact/scripts/node_tests/genome_original.bin";
-    genome_original->write_to_file(path);
-    RNN_Genome* genome_from_file = new RNN_Genome(path);
-
+    
     int32_t num_weights = genome_original->get_number_weights();
-    vector<double> weights;
-    generate_random_vector(num_weights, weights);
+    vector<double> best_parameters_original;
+    generate_random_vector(num_weights, best_parameters_original);
+    genome_original->set_best_parameters(best_parameters_original);
+    
+    
+    string path = "./genome_original.bin";
+    genome_original->write_to_file(path);
+    RNN_Genome* genome_file = new RNN_Genome(path);
+    vector<double> best_parameters_file = genome_file->get_best_parameters();
 
+    if (best_parameters_original == best_parameters_file){
+        Log::info("PASS: BEST PARAMETERS ARE EQUAL!!!\n");
+    } else {
+        Log::info("FAILURE: BEST PARAMETERS ARE NOT EQUAL!!!\n");
+    }
+              
     RNN* rnn_original = genome_original->get_rnn();
-    RNN* rnn_from_file = genome_from_file->get_rnn();
+    RNN* rnn_file = genome_file->get_rnn();
 
     double analytic_mse_original, analytic_mse_file;
     vector<double> analytic_gradient_original, analytic_gradient_file;
 
     rnn_original->get_analytic_gradient(
-        weights, inputs, outputs, analytic_mse_original, analytic_gradient_original, false, true, 0.0
+        best_parameters_original, inputs, outputs, analytic_mse_original, analytic_gradient_original, false, true, 0.0
     );
-    rnn_from_file->get_analytic_gradient(
-        weights, inputs, outputs, analytic_mse_file, analytic_gradient_file, false, true, 0.0
+    rnn_file->get_analytic_gradient(
+        best_parameters_file, inputs, outputs, analytic_mse_file, analytic_gradient_file, false, true, 0.0
     );
 
-    if (rnn_original->get_number_nodes() == rnn_from_file->get_number_nodes()) {
+    if (rnn_original->get_number_nodes() == rnn_file->get_number_nodes()) {
         Log::info("PASS: SAME NODE COUNT!!!\n");
     } else {
         Log::info("FAILURE: DIFFERENT NODE COUNT!!!\n");
     }
+    if (rnn_original->get_number_edges() == rnn_file->get_number_edges()) {
+        Log::info("PASS: SAME EDGE COUNT!!!\n");
+    } else {
+        Log::info("FAILURE: DIFFERENT EDGE COUNT!!!\n");
+    }
+    int output_node_count = 1;
     for (int i = 0; i < rnn_original->get_number_nodes(); i++) {
         RNN_Node_Interface* test_node_original = rnn_original->get_node(i);
-        RNN_Node_Interface* test_node_from_file = rnn_from_file->get_node(i);
-        if ((test_node_original->layer_type == OUTPUT_LAYER) && (test_node_from_file->layer_type == OUTPUT_LAYER)) {
-            if (test_node_original->output_values == test_node_from_file->output_values) {
-                Log::info("PASS: FORWARD PASS OUTPUTS ARE THE SAME!!!\n");
+        RNN_Node_Interface* test_node_file = rnn_file->get_node(i);
+        if ((test_node_original->layer_type == OUTPUT_LAYER) && (test_node_file->layer_type == OUTPUT_LAYER)) {
+            if (test_node_original->output_values == test_node_file->output_values) {
+                Log::info("PASS: FORWARD PASS OUTPUT NODE %d - OUTPUT IS THE SAME!!!\n", output_node_count);
             } else {
-                Log::info("FAILURE: FORWARD PASS OUTPUTS NOT EQUAL!!!\n");
+                Log::info("FAILURE: FORWARD PASS OUTPUT NODE %d - OUTPUT IS NOT EQUAL!!!\n", output_node_count);
             }
+            ++output_node_count;
         }
     }
 
@@ -105,4 +132,5 @@ int main(int argc, char** argv) {
     }
 
     delete genome_original;
+    delete genome_file;
 }
--- scripts/node_tests/test_node_to_binary.sh
+++ scripts/node_tests/test_node_to_binary.sh
@@ -2,11 +2,9 @@
 #This will do a test that converts a genome to binary, imports the genome back from binary as a new object, 
 #and checks for forward pass output equivilence and backward pass gradient equivilence between the two  
 
-find ~/exact/scripts/node_tests -type f -name "*.bin" -exec rm {} +
-
-cd ~/exact/build
+find ./ -type f -name "genome_original.bin" -exec rm {} +
 
 #for sin node
-./rnn_tests/test_node_to_binary --output_directory results_gradient_check --hidden_node_type sin --std_message_level INFO --file_message_level NONE
+./build/rnn_tests/test_node_to_binary --output_directory results_gradient_check --hidden_node_type sin --std_message_level INFO --file_message_level NONE
 
-find ~/exact/scripts/node_tests -type f -name "*.bin" -exec rm {} +
+find ./ -type f -name "genome_original.bin" -exec rm {} +
