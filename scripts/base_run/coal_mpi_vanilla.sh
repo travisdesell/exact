5f69694a3a6f7401a303def30f8a2f74594acf2d
VelDia <sonechkodiana02@gmail.com>
2025-09-18 12:44:08 -0400

    changes to run line and grid search

--- scripts/base_run/coal_mpi_vanilla.sh
+++ scripts/base_run/coal_mpi_vanilla.sh
@@ -9,36 +9,42 @@
 
 # Number of runs can be provided as the first argument, defaults to 1
 RUNS=${1:-1}
+# Line search values for bp_iterations (space-separated)
+BPS=${2:-"0 1 5 10 25 50"}
 
 cd build
 
 INPUT_PARAMETERS="Conditioner_Inlet_Temp Conditioner_Outlet_Temp Coal_Feeder_Rate Primary_Air_Flow Primary_Air_Split System_Secondary_Air_Flow_Total Secondary_Air_Flow Secondary_Air_Split Tertiary_Air_Split Total_Comb_Air_Flow Supp_Fuel_Flow Main_Flm_Int" 
 OUTPUT_PARAMETERS="Main_Flm_Int" 
 
-for i in $(seq 1 $RUNS); do
-exp_name="../test_output/coal_mpi_vanilla/run_${i}"
-mkdir -p "$exp_name"
-echo "Run ${i}/${RUNS}: results will be saved to: $exp_name"
-echo "###-------------------###"
+for bp in $BPS; do
+  echo "=== bp_iterations: $bp ==="
+  for i in $(seq 1 $RUNS); do
+    exp_name="../test_output/line_grid_search/coal_mpi_vanilla/bp_${bp}/run_${i}"
+    mkdir -p "$exp_name"
+    echo "Run ${i}/${RUNS} (bp_iterations=$bp): results will be saved to: $exp_name"
+    echo "###-------------------###"
 
-mpirun -np 4 ./mpi/examm_mpi \
---training_filenames ../datasets/2018_coal/burner_[0-9].csv --validation_filenames ../datasets/2018_coal/burner_1[0-1].csv \
---time_offset 1 \
---input_parameter_names $INPUT_PARAMETERS \
---output_parameter_names $OUTPUT_PARAMETERS \
---number_islands 10 \
---island_size 10 \
---max_genomes 2000 \
---bp_iterations 25 \
---backprop_iterations_type "const" \
---output_directory "$exp_name" \
---num_mutations 2 \
---weight_update adagrad \
---eps 0.000001 \
---beta1 0.99 \
---sequence_length 50 \
---possible_node_types simple UGRNN MGU GRU delta LSTM \
---save_genome_option the_best \
---std_message_level INFO \
---file_message_level INFO
+    mpirun -np 4 ./mpi/examm_mpi \
+    --training_filenames ../datasets/2018_coal/burner_[0-9].csv --validation_filenames ../datasets/2018_coal/burner_1[0-1].csv \
+    --time_offset 1 \
+    --input_parameter_names $INPUT_PARAMETERS \
+    --output_parameter_names $OUTPUT_PARAMETERS \
+    --number_islands 10 \
+    --island_size 10 \
+    --max_genomes 2000 \
+    --max_wallclock_seconds 1200 \
+    --bp_iterations $bp \
+    --backprop_iterations_type "const" \
+    --output_directory "$exp_name" \
+    --num_mutations 2 \
+    --weight_update adagrad \
+    --eps 0.000001 \
+    --beta1 0.99 \
+    --sequence_length 50 \
+    --possible_node_types simple UGRNN MGU GRU delta LSTM \
+    --save_genome_option the_best \
+    --std_message_level INFO \
+    --file_message_level INFO
+  done
 done
