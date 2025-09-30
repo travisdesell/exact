4653a88a9c53f14cd6359079c0e9e5091415e70f
Diana Velychko (RIT Student) <dv6943@sporcsubmit.rc.rit.edu>
2025-09-30 13:24:59 -0400

    Fixed the authentification issue to submit jobs

--- scripts/base_run/coal_mpi_bp_sweep_sbatch.sh
+++ scripts/base_run/coal_mpi_bp_sweep_sbatch.sh
@@ -6,12 +6,11 @@
 #SBATCH -t 4-23:00:00
 #SBATCH --output=%n_%x_%j.out
 #SBATCH --error=%x_%a_%j.err
-#SBATCH -A cps
 #SBATCH --partition=tier3
 #SBATCH --nodes=1			# How many nodes to run on
-#SBATCH --ntasks=1			# How many tasks per node
-#SBATCH --cpus-per-task=18		# Number of CPUs per task
-#SBATCH --mem-per-cpu=10g		# Memory per CPU
+#SBATCH --ntasks=18			# How many tasks per node
+#SBATCH --cpus-per-task=36		# Number of CPUs per task
+#SBATCH --mem-per-cpu=64g		# Memory per CPU
 #SBATCH --gres=gpu:a100:1
 
 hostname
