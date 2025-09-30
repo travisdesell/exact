ba823cc680aa1467e841cb9564d9c091619a13f0
VelDia <sonechkodiana02@gmail.com>
2025-09-30 05:43:35 -0400

    cpus

--- scripts/base_run/coal_mpi_bp_sweep_sbatch.sh
+++ scripts/base_run/coal_mpi_bp_sweep_sbatch.sh
@@ -10,7 +10,7 @@
 #SBATCH --partition=tier3
 #SBATCH --nodes=1			# How many nodes to run on
 #SBATCH --ntasks=36			# How many tasks per node
-#SBATCH --cpus-per-task=36		# Number of CPUs per task
+#SBATCH --cpus-per-task=2		# Number of CPUs per task
 #SBATCH --mem-per-cpu=10g		# Memory per CPU
 #SBATCH --gres=gpu:a100:1
 
