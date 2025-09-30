8e8f9752736556904371c3bdab86036336d3de54
VelDia <sonechkodiana02@gmail.com>
2025-09-30 13:35:59 -0400

    test cluster  job submission

---
+++ scripts/base_run/test_job_submission.py
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -J neuroevol_bp_iterations
+#SBATCH --account=neuroevolution
+#SBATCH --mail-user=slack:@dv6943
+#SBATCH --mail-type=ALL
+#SBATCH -t 0-00:05:00
+#SBATCH --output=%n_%x_%j.out
+#SBATCH --error=%x_%a_%j.err
+#SBATCH --partition=tier3
+#SBATCH --nodes=1			# How many nodes to run on
+#SBATCH --ntasks=18			# How many tasks per node
+#SBATCH --cpus-per-task=36		# Number of CPUs per task
+#SBATCH --mem-per-cpu=64g		# Memory per CPU
+#SBATCH --gres=gpu:a100:1
+
+hostname
