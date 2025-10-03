d0e3a86c13b59d6e616b67c36d43f3fe300a779d
Diana Velychko (RIT Student) <dv6943@sporcsubmit.rc.rit.edu>
2025-10-03 13:43:41 -0400

    eliminating unnecessary lines

--- scripts/backprop_growth_tests/test_random/sbatch_coal_mpi_random_24_40_1.sh
+++ scripts/backprop_growth_tests/test_random/sbatch_coal_mpi_random_24_40_1.sh
@@ -10,10 +10,8 @@
 #SBATCH -n 18
 #SBATCH --mem-per-cpu=10g
 
-cd /home/dv6943/exact
-source /home/dv6943/new_env/bin/activate
-spack load gcc/lhqcen5
-spack load libtiff/gnxev37
-spack load openmpi/xcunp5q
+spack load gcc
+spack load libtiff
+spack load openmpi
 
 sh scripts/backprop_growth_tests/test_random/coal_mpi_random_24_40_1.sh
