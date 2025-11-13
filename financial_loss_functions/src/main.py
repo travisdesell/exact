1cfd196cc7958c2c9d3b827afa04ef5b5a448f7a
rahulkfernandes <rahulkfernandes@gmail.com>
2025-11-12 19:07:08 -0500

    cleaning function added

--- financial_loss_functions/src/main.py
+++ financial_loss_functions/src/main.py
@@ -1,7 +1,12 @@
 import os
 from dotenv import load_dotenv
+<<<<<<< HEAD:financial_loss_functions/src/main.py
 from src.models.cov_models import HierarchialRiskParity
 from src.data_processing.preprocess import load_crsp_datasets, clean_data_returns, preprocess_cov
+=======
+from cov_models import HierarchialRiskParity
+from preprocess import load_crsp_datasets, get_only_returns, preprocess_cov, clean_inplace
+>>>>>>> ad232f2 (cleaning function added):financial_loss_functions/main.py
 
 load_dotenv()
 
@@ -13,15 +18,21 @@ if __name__ == '__main__':
     train_data, val_data, test_data = load_crsp_datasets(crsp_path)
 
     # -------------------- Cleaning & Processing -------------------- #
-    train_ret, val_ret, test_ret = clean_data_returns(
-        train_data,
-        val_data,
-        test_data
-    )
+    # Clean dataset inplace
+    print(train_data.shape)    
+    clean_inplace(train_data, val_data, test_data)
+    print(train_data.shape)
+    
+    
+    # train_ret, val_ret, test_ret = get_only_returns(
+    #     train_data,
+    #     val_data,
+    #     test_data
+    # )
 
-    cov, corr = preprocess_cov(train_ret)
+    # cov, corr = preprocess_cov(train_ret)
 
-    # -------------------- Modeling -------------------- #
-    hrp = HierarchialRiskParity()
-    weights = hrp.calculate_weights(cov, corr)
-    print(weights * 100)
+    # # -------------------- Modeling -------------------- #
+    # hrp = HierarchialRiskParity()
+    # weights = hrp.calculate_weights(cov, corr)
+    # print(weights * 100)
