35024ce3ee5b0c415e1fc8efd9fa2e342753cfbf
rahulkfernandes <rahulkfernandes@gmail.com>
2026-01-19 11:29:30 -0500

    rms_sortino added

--- financial_loss_functions/src/training/loss_functions.py
+++ financial_loss_functions/src/training/loss_functions.py
@@ -1,9 +1,11 @@
-from torch import clamp, tensor
+from torch import clamp, tensor, sqrt, mean
 
 def raw_sharpe_loss(
         weights: tensor, returns: tensor, eps: float = 1e-8
     ):
     """
+    Raw Sharpe ratio using standard deviation directly.
+
     @param weights torch.tensor (B, N)
     @param returns torch.tensor (B, T_out, N) -- raw returns
 
@@ -22,6 +24,16 @@ def raw_sharpe_loss(
 def differentiable_sharpe_loss(
         weights: tensor, returns: tensor, eps: float = 1e-6
     ):
+    """
+    Differentiable Sharpe ratio, where we calculate square root of variance 
+    instead of standard deviation.
+    
+    @param weights torch.tensor (B, N)
+    @param returns torch.tensor (B, T_out, N) -- raw returns
+
+    @return batch average Sharpe Ratio. 
+        Negative since NN has to maximize Sharpe Ratio but minimize loss
+    """
     port_ret = (weights.unsqueeze(1) * returns).sum(-1)   # (B, T)
     mean = port_ret.mean(dim=1)
     var  = port_ret.var(dim=1)          # variance, not std
@@ -49,8 +61,35 @@ def raw_sortino_loss(
     downside = clamp(target - port, min=0.0)  # (B, T_out), only positive for downside
     downside_std = downside.std(dim=1) + eps  # (B,)
     
-    mean = port.mean(dim=1)  # (B,)
-    sortino = mean / downside_std  # (B,)
+    mean_return = port.mean(dim=1)  # (B,)
+    sortino = mean_return / downside_std  # (B,)
     
-    # Maximize Sortino → minimize negative Sortino
+    # Maximize Sortino -> minimize negative Sortino
+    return -sortino.mean()
+
+def rms_sortino_loss(
+        weights: tensor, returns: tensor, target: float = 0.0, eps: float = 1e-8
+    ):
+    """
+    @param weights torch.tensor (B, N)
+    @param returns torch.tensor (B, T_out, N) -- raw returns
+    @param target float Minimum acceptable return (MAR), often 0 for risk-free rate adjusted.
+    @param eps float Epsilon value to avoid divide by zero error
+
+    @return batch average Sortino Ratio. 
+        Negative since NN has to maximize Sortino but minimize loss
+    """
+    # Portfolio returns per step
+    port = (weights.unsqueeze(1) * returns).sum(dim=-1)  # (B, T_out)
+    
+    # Downside deviation: std of negative deviations from target
+    downside = clamp(target - port, min=0.0)  # (B, T_out), only positive for downside
+    
+    # RMS downside: sqrt(mean(downside**2) + eps) -> for stable gradients
+    downside_rms = sqrt(mean(downside ** 2, dim=1) + eps)  # (B,)
+    
+    mean_return = port.mean(dim=1)  # (B,)
+    sortino = mean_return / downside_rms  # (B,)
+    
+    # Maximize Sortino -> minimize negative Sortino
     return -sortino.mean()
