e7e7584dfe9d251864a4991f7208ea0852a4847b
rahulkfernandes <rahulkfernandes@gmail.com>
2026-04-16 15:33:00 -0400

    Feat: Saves daily returns as benchs & nn separated

--- financial_loss_functions/src/evaluation/pipeline.py
+++ financial_loss_functions/src/evaluation/pipeline.py
@@ -329,7 +329,10 @@ def run_evaluation_pipeline(
     )
     
     # -------------------- Combining and Savining All Returns -------------------- # 
-    all_daily_rets_w_dates = nn_daily_rets_w_dates | bench_daily_rets_w_dates # TODO: Refactor to include "nn_models" as key
+    all_daily_rets_w_dates = {
+        'nn_models': nn_daily_rets_w_dates,
+        'benchmarks': bench_daily_rets_w_dates
+    }
 
     all_rets_file_name = artifacts_paths['test_perf_dir'] \
         / f'test_returns_{results_suffix}.json'
