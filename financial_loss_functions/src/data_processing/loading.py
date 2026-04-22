b64f9ac7a99b2a7a897960832b5fbdc6520cb1c2
rahulkfernandes <rahulkfernandes@gmail.com>
2026-04-21 20:11:04 -0400

    Docs: Docstrings updated for loading.py

--- financial_loss_functions/src/data_processing/loading.py
+++ financial_loss_functions/src/data_processing/loading.py
@@ -10,12 +10,12 @@ def load_raw_crsp_datasets(
     validation and test.
 
     Args:
-        train_path (str): Path to raw train data file
-        val_path (str): Path to raw validation data file
-        test_path (str): Path to raw test data file
+        train_path (str): Path to raw train data file.
+        val_path (str): Path to raw validation data file.
+        test_path (str): Path to raw test data file.
     
     Returns:
-        tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: Raw train, val and test data
+        tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: Raw train, val and test data.
     """ 
     # Load split datasets
     train_data = pd.read_csv(train_path)
@@ -91,15 +91,25 @@ def load_macro_data(macro_dir_path: str | Path) -> dict[str, pd.DataFrame]:
 
 class ArtifactDataExtractor:
     """
-    Class to extract artifacts data from the artificats directory. Can extract, average performances,
+    Class to extract artifacts data from the artifacts directory. Can extract average performances,
     optimized hyperparameters and daily return performances.
     """
     def __init__(
             self,
             prev_grid_mode: str,
             artifacts_paths: dict[str, Path | str]
-
         ):
+        """
+        Initialize ArtifactsExtractor instatnce to extract files like daily returns, 
+        average portfolio performances and optimized hyperparameter from artifacts directory. 
+
+        Args:
+            prev_grid_mode (str): Grid mode used in the previous pipeline. Either 'one_model' or 'one'.
+            artifacts_paths (dict[str, Path | str]): Dictionary containing paths of artifact files.
+        
+        Raises:
+            ValueError: If Incorrect mode is provided. Must be 'one_model' or 'one'.
+        """
         self.prev_grid_mode = prev_grid_mode
         self.artifacts_paths = artifacts_paths
 
@@ -109,6 +119,18 @@ class ArtifactDataExtractor:
     def find_artifact_files(
         self, prefix: str, suffixes: list[str], dir_path: str | Path, ext: str
     ) -> dict[str, str]:
+        """
+        Find the artifact files in the directory and return a dict with files and thei existence status.
+
+        Args:
+            prefix (str): String prefix given to files while saving them.
+            suffixes (str): List of model name suffixes given while saving files.
+            dir_path (str | Path): Path to directory being searched for the files.
+            ext (str): File extentions like '.csv' or '.json'.
+        
+        Returns:
+            arti_paths (dict[str, str]): Dictonary containing existence status and file paths. 
+        """
         paths_temp = []
         for suff in suffixes:
             paths_temp.append(
@@ -132,6 +154,21 @@ class ArtifactDataExtractor:
     def agg_avg_perf(
             self, avg_perf_prefix: str, model_names: list[str]
         ) -> pd.DataFrame:
+        """
+        Load and aggregate average model portfolio performance files using the given prefix 
+        and list of model names (suffixes).
+
+        Args:
+            avg_perf_prefix (str): Prefix for the average performance files.
+            model_names (list[str]): List of model names which are the suffix to their files.
+        
+        Returns:
+            all_avg_perf (pd.DataFrame): Dataframe containing aggregated average model portfolio performances.
+        
+        Raises:
+            RuntimeError: If no average performance files are found.
+            ValueError: This method does not work when previoud grid mode is 'one'.
+        """
         if self.prev_grid_mode == 'one_model':
             # Build paths
             avg_perf_paths = self.find_artifact_files(
@@ -160,8 +197,26 @@ class ArtifactDataExtractor:
     def agg_opti_hparams(
             self, opti_hparams_prefix: str, model_names: list[str]
         ) -> dict:
+        """
+        Load and aggregate optimized hyperparemeter json files using the given prefix 
+        and list of model names (suffixes).
+
+        Args:
+            opti_hparams_prefix (str): Prefix for the optimized hyperparameter files.
+            model_names (list[str]): List of model names which are the suffix to their files.
+        
+        Returns:
+            optimized_hparams (dict): Dictionary containing the optimized hyperparemeters 
+                for each model loss combination.
+        
+        Raises:
+            ValueError: If more than one model+loss combination is present in the model_names list, 
+                when previous grid mode is 'one'.
+        """
         if self.prev_grid_mode == 'one' and len(model_names) != 1:
-            raise ValueError('Provided grid mode is `one`, but more than one model-loss provided.')
+            raise ValueError(
+                'Provided grid mode is `one`, but more than one model-loss provided.'
+            )
         
         # Build paths
         opti_paths = self.find_artifact_files(
@@ -183,11 +238,21 @@ class ArtifactDataExtractor:
             )
             optimized_hparams = None
 
-        
         return optimized_hparams
     
     def agg_daily_rets(self, rets_prefix: str, model_names: list[str]) -> dict:
+        """
+        Load and aggregate the daily returns files using the given prefix 
+        and list of model names (suffixes).
 
+        Args:
+            rets_prefix (str): Prefix for the daily returns files.
+            model_names (list[str]): List of model names which are the suffix to their files.
+
+        Returns:
+            daily_rets (dict): Dictionary containing the daily returns for each model+loss 
+                combination for each evaluation window.
+        """
         # Build paths
         rets_paths = self.find_artifact_files(
             rets_prefix,
